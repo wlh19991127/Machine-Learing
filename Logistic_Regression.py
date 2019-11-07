9dab9ae272a9e036bcd4c10a7c8efbd54e169e6b
wlh19991127 <52598772+wlh19991127@users.noreply.github.com>
2019-11-07 20:40:19 +0800

    Update and rename SF_2.py to Logistic_Regression.py

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
+'''
+author:wlh19991127
+机器学习算法2：逻辑回归 
+包含正则化、特征映射
 
-# In[214]:
+'''
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
 
+#读取文件
 def read_txt(filename,split):
     data=np.loadtxt(filename,delimiter=split,dtype=np.float64)
-    x=data[:,0:-1]
-    y=data[:,-1]
+    x=data[:,0:-1]  #数据集
+    y=data[:,-1]    #结果集
     return x,y
 
+#画散列图 只能针对两个属性的数据集
 def plot_scatt(x,y):
     x1=x[:,0]
     x2=x[:,1]
@@ -23,9 +29,11 @@ def plot_scatt(x,y):
         else:
             plt.scatter(x1[i],x2[i],marker='x',color='red')
 
+#S函数，将假设函数映射到[0,1]区间
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
 
+#特征映射，将属性映射为更高次幂  power为最高次幂
 def feathure_map(x1,x2,power):
     m=x1.shape[0]
     X=np.ones((m,1))
@@ -35,6 +43,7 @@ def feathure_map(x1,x2,power):
             X=np.hstack((X,x_temp.reshape((m,1))))
     return X
 
+#计算代价函数，lamda为可选项，指定lamda的值则为正则化
 def calculate_cost(theta,X,y,lamda=0):
     j=0
     m=len(y)
@@ -46,6 +55,7 @@ def calculate_cost(theta,X,y,lamda=0):
     j=(first+second)/(-m)+(lamda/(2*m))*theta1.T@theta1
     return j
 
+#计算梯度，lamda为可选项，指定lamda则为正则化
 def gradient(theta,X,y,lamda=0):
     m=len(y)
     h=sigmoid(X@theta)
@@ -54,57 +64,58 @@ def gradient(theta,X,y,lamda=0):
     grad=(X.T@(h-y))/m+lamda/m*theta1
     return grad
 
-def predict(x,theta):
+#预测函数
+def predict(x,theta,power=1):
     X=np.hstack((np.ones((1,1)),x))
-    X=feathure_map(X[:,1],X[:,2],6)
-    return sigmoid(X@theta)
+    X=feathure_map(X[:,1],X[:,2],power)
+    pred=sigmoid(X@theta)
+    if pred>=0.5:
+        print("预测结果为1,准确率为:",pred)
+    else:
+        print("预测结果为0,准确率为:",1-pred)
 
-def decision_boundary(theta):
-   # u=np.linspace(-1,1.5,50)
-   # v=np.linspace(-1,1.5,50)
-  #  z=np.zeros((len(u),len(v)))
-   # for i in range(len(u)):
-   #     for j in range(len(v)):
-            #print(i,j)
-    #        z[i][j]=feathure_map(u[i].reshape(1,1),v[i].reshape(1,1),6)@theta
-  #  z=z.T
-   # plt.contour(u,v,z,0)
+#作出决策边界
+def decision_boundary(theta,power):
     u = np.linspace(-1,1.5,50)  #根据具体的数据，这里需要调整
     v = np.linspace(-1,1.5,50)
     z = np.zeros((len(u),len(v)))
     for i in range(len(u)):
         for j in range(len(v)):
-            z[i,j] = np.dot(feathure_map(u[i].reshape(1,-1),v[j].reshape(1,-1),10),theta)    # 计算对应的值，需要map
+            z[i,j] = np.dot(feathure_map(u[i].reshape(1,1),v[j].reshape(1,1),power),theta)    # 计算对应的值，需要map
     z = np.transpose(z)
     plt.contour(u,v,z,0) 
 
-def run():
+#运行
+def run(lamda=0,power=1):
     x,y=read_txt('C:/Users/wang/Desktop/机器学习编程/machine-learning-ex2/ex2/ex2data2.txt',',')
     m=len(y) #数据的条数 100
     y=y.reshape((m,1))
     plot_scatt(x,y) #画出散点图
     X=np.hstack((np.ones((m,1)),x)) #X矩阵
-    X=feathure_map(X[:,1],X[:,2],10)
+    X=feathure_map(X[:,1],X[:,2],power)
     args=X.shape[1] #特征值的数量 2
     theta=np.zeros((args,1))
-    #print(theta)
     cost_start=calculate_cost(theta,X,y)
-    alpha=0.01
-   # print(gradient(theta,X,y))
     print("初始代价函数的值为",cost_start)
-   # result=optimize.fmin_bfgs(f=calculate_cost,x0=theta,fprime=gradient,args=(X,y))
-    #print(result)
-    print(X.shape,theta.shape,y.shape)
+    
+    '''
+        fmin_tnc()函数用于求解指定函数的最小值，第一个参数为要求解最小值的函数，第二个参数为初始化的参数，第三个参数为
+        梯度函数，第四个参数是一个参数列表 要注意传入的数据类型
+    '''
+    
     theta=theta.reshape((args))
     y=y.reshape((y.shape[0]))
-    #result =optimize.fmin_tnc(func=calculate_cost,x0=theta,fprime=gradient,args=(X,y))
-    result=optimize.fmin_bfgs(calculate_cost,theta,fprime=gradient,args=(X,y,1))
-    #print(result)
-    #print(predict(np.array([0.25,0.25]).reshape(1,2),result))
-    #X_fm=feathure_map(X[:,1],X[:,2],6)
-    decision_boundary(result)
+    result=optimize.fmin_bfgs(calculate_cost,theta,fprime=gradient,args=(X,y,lamda))
+    cost_end=calculate_cost(theta,X,y,lamda)
+    print("最终的代价函数的值为",cost_end)
+    decision_boundary(result,power)
+    pred_x=np.array((0.25,0.25)).reshape((1,2))
+    predict(pred_x,result,power)
+    
+if __name__=='__main__':
+    run(lamda=1,power=6)
     
     
 if __name__=='__main__':
-    run()
+    run(lamda=1,power=6)
 
